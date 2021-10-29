24fafb2493c9ab576144c470047603453f97c18f
José Luis Tello <jltello96@gmail.com>
2021-10-29 00:54:24 -0500

    Adds

--- kkoma/settings.py
+++ kkoma/settings.py
@@ -46,13 +46,14 @@ INSTALLED_APPS = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000/",
+    "http://localhost:8000",
 ]
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
