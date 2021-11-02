9a1b901a64862d81e0a8ad7582e67f841fcc6126
José Luis Tello <jltello96@gmail.com>
2021-11-01 19:59:49 -0600

    test

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Category 
 
-# Create your tests here.
+class CategoryModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Category.objects.create(name='first_name', slug='a slug here')
+
+    def test_name_content(self):
+        category = Category.objects.get(id=1)
+        expected_object_name = f'{category.name}'
+        self.assertEqual(expected_object_name, 'first_name')
+
+    def test_slug_content(self):
+        category = Category.objects.get(id=1)
+        expected_object_name = f'{category.slug}'
+        self.assertEqual(expected_object_name, 'a slug here')
+
+class ProductModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Category.objects.create()
