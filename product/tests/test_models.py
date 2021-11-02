4668eb78254ac33da84c537d9a12aecc2a0cc1ba
José Luis Tello <jltello96@gmail.com>
2021-11-02 00:48:08 -0600

    test

---
+++ product/tests/test_models.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+from models import Category, Product
+
+class CategoryModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Category.objects.create(name='first_name', slug='a slug here')
+
+    def test_name_content(self):
+        category = Category.objects.get(id=1)
+        expected_object_name = f'{category.name}'
+        self.assertEqual(expected_object_name, 'first_name')
+
+    def test_slug_content(self):
+        category = Category.objects.get(id=1)
+        expected_object_name = f'{category.slug}'
+        self.assertEqual(expected_object_name, 'a slug here')
+
+class ProductModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Product.objects.create(category='Bebidas', name='Pepsi', slug='/Pepsi-600-ml', description='Compre en paquetes de 6', price='$600', image='pepsi.jpge', thumbnail='pepsi1.jpge', data_added='500')
+
+    def test_category_content(self):
+        product = Product.objects.get(id=1)
+        expected_object_name = f'{product.}'
+        self.assertEqual(expected_object_name, '')
+
+    def test_category_content(self):
+        product = Product.objects.get(id=1)
+        expected_object_name = f'{product.}'
+        self.assertEqual(expected_object_name, '')
+
+    def test_category_content(self):
+        product = Product.objects.get(id=1)
+        expected_object_name = f'{product.}'
+        self.assertEqual(expected_object_name, '')
+
+    def test_category_content(self):
+        product = Product.objects.get(id=1)
+        expected_object_name = f'{product.}'
+        self.assertEqual(expected_object_name, '')    
+
+    def test_category_content(self):
+        product = Product.objects.get(id=1)
+        expected_object_name = f'{product.}'
+        self.assertEqual(expected_object_name, '')
+
+    def test_category_content(self):
+        product = Product.objects.get(id=1)
+        expected_object_name = f'{product.}'
+        self.assertEqual(expected_object_name, '')
+
+    def test_category_content(self):
+        product = Product.objects.get(id=1)
+        expected_object_name = f'{product.}'
+        self.assertEqual(expected_object_name, '')
+
+    def test_category_content(self):
+        product = Product.objects.get(id=1)
+        expected_object_name = f'{product.}'
+        self.assertEqual(expected_object_name, '')
+    
