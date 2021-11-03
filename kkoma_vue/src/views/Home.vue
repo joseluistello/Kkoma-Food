57b8995aa678635f6af65a6fba20833efd240453
José Luis Tello <jltello96@gmail.com>
2021-11-03 05:09:34 -0600

    adds

--- kkoma_vue/src/views/Home.vue
+++ kkoma_vue/src/views/Home.vue
@@ -13,7 +13,7 @@
 
     <div class="columns is-multiline">
       <div class="column is-12">
-          <h2 class="is-size-2 has-text-centered">Ultimos productos</h2>
+          <h2 class="is-size-2 has-text-centered">Latest products</h2>
       </div>
 
       <div
@@ -21,7 +21,6 @@
         v-for="product in latestProducts"
         v-bind:key="product.id"
       >
-
         <div class="box">
           <figure class="image mb-4">
             <img :src="product.get_thumbnail">
@@ -31,7 +30,6 @@
           <p class="is-size-6 has-text-grey">${{ product.price }}</p>
 
           View details
-
         </div>
       </div>
     </div>
@@ -53,10 +51,10 @@ export default {
   mounted() {
     this.getLatestProducts()
   },
-  methds: {
+  methods: {
     getLatestProducts() {
       axios
-        .get('/api/v1/latest-products')
+        .get('/api/v1/latest-products/')
         .then(response => {
           this.latestProducts = response.data
         })
@@ -67,7 +65,6 @@ export default {
   }
 }
 </script>
-
 
 <style scoped>
   .image {
--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from django.urls.resolvers import URLPattern
+
 
 from product import views
 
 urlpatterns = [
-    path('latest-products/', views.LatestProductList.as_view())
+    path('latest-products/', views.LatestProductList.as_view()),
 ]
