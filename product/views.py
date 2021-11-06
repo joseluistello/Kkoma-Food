53ee2b8d40625c05e0f6668412c519c1503fe07b
José Luis Tello <jltello96@gmail.com>
2021-11-05 19:00:36 -0600

    adds

--- product/views.py
+++ product/views.py
@@ -17,13 +17,12 @@ class LatestProductList(APIView):
 class ProductDetail(APIView):
     def get_object(self, category_slug, product_slug):
         try:
-            return Product.objects.filter(Category=category_slug).get(slug=product_slug)
+            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
         except Product.DoesNotExist:
             raise Http404
-
-    def get_object(self, category_slug, product_slug, format=None):
+    
+    def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
-        
