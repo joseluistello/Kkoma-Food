52d33120d5fd49deab623e7c44ba994921b53dc1
José Luis Tello <jltello96@gmail.com>
2021-11-15 01:26:32 -0600

    adds

--- kkoma_vue/src/views/Home.vue
+++ kkoma_vue/src/views/Home.vue
@@ -52,8 +52,10 @@ export default {
     this.getLatestProducts()
   },
   methods: {
-    getLatestProducts() {
-      axios
+    async getLatestProducts() {
+      this.$store.commit('setIsLoading', true)
+
+      await axios
         .get('/api/v1/latest-products/')
         .then(response => {
           this.latestProducts = response.data
@@ -61,6 +63,8 @@ export default {
         .catch(error => {
           console.log(error)
         })
+
+      this.$store.commit('setIsLoading', false)
     }
   }
 }
