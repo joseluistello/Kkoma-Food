32a730983a909ff8389359a5d2c86aaed2a90990
José Luis Tello <jltello96@gmail.com>
2021-11-16 00:58:46 -0600

    views

--- product/views.py
+++ product/views.py
@@ -3,16 +3,18 @@ from django.shortcuts import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Product
+from .models import Category, Product
 from .serializers import ProductSerializer
 
+
+### Te permite crear y mostrar una lista de productos en home
 class LatestProductList(APIView):
     def get(self, request, format=None):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-
+### Te permite ver cada producto de manera aislada
 class ProductDetail(APIView):
     def get_object(self, category_slug, product_slug):
         try:
@@ -24,3 +26,14 @@ class ProductDetail(APIView):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+
+### Te permite ver una lista de productos de acuerdo a su categoria
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Product.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, format=None):
+        category = se
