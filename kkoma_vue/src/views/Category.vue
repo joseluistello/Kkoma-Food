408d6a3b473975432426ec2021d1be427d21d278
José Luis Tello <jltello96@gmail.com>
2021-11-17 22:12:18 -0600

    adds

--- kkoma_vue/src/views/Category.vue
+++ kkoma_vue/src/views/Category.vue
@@ -4,6 +4,24 @@
             <div class="column is-12">
                 <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
             </div>
+
+            <div
+                class="column is-3"
+                v-for="product in category.products"
+                v-bind:key="product.id"
+            >
+                <div class="box">
+                <figure class="image mb-4">
+                    <img :src="product.get_thumbnail">
+                </figure>
+
+                <h3 class="is-size-4">{{ product.name }}</h3>
+                <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+                <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+                </div>
+            </div> 
+
         </div>
     </div>
 </template>
