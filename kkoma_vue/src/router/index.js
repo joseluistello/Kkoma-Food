d3e41ff1fc3f4405fe41c8202f4e5995df12bec9
José Luis Tello <jltello96@gmail.com>
2021-11-25 14:42:11 -0600

    otropinchebug

--- kkoma_vue/src/router/index.js
+++ kkoma_vue/src/router/index.js
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+import store from '../store'
+
 import Home from '../views/Home.vue'
 
 import Product from '../views/Product.vue'
@@ -7,6 +10,7 @@ import Search from '../views/Search.vue'
 import Cart from '../views/Cart.vue'
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import MyAccount from '../views/MyAccount.vue'
 
 const routes = [
   {
@@ -33,6 +37,14 @@ const routes = [
     component: LogIn
   },
   {
+    path: '/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+        requiredLogin: true
+    }
+  },
+  {
     path: '/search',
     name: 'Search',
     component: Search
@@ -57,6 +69,15 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiredLogin) && !store.state.isAuthenticated) {
+    next({ name: 'LogIn', query: { to: to.path } });
+  } else {
+    next()
+  }
 })
 
 export default router
