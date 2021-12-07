5e4d54c50ba1a97678c062d08224c10c0216df9c
José Luis Tello <jltello96@gmail.com>
2021-12-07 03:50:55 -0600

    fuckingauthenticationpieceofstripe

--- kkoma/wsgi.py
+++ kkoma/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kkoma.settings')
 
 application = get_wsgi_application()
+
--- kkoma_vue/src/views/CheckOut.vue
+++ kkoma_vue/src/views/CheckOut.vue
@@ -110,7 +110,7 @@
                 <template v-if="cartTotalLength">
                     <hr>
 
-                    <button class="button is-dark" @click="submitForm">Pay with Stripe</button>
+                    <button class="button is-dark" @click="submitForm">Paga con Stripe</button>
                 </template>
                 
             </div>
@@ -142,9 +142,17 @@ export default {
         }
     },
     mounted() {
-        document.title = 'Lista | Kkoma'
+        document.title = ' Lista | Kkoma '
 
         this.cart = this.$store.state.cart
+
+        if (this.cartTotalLength > 0) {
+            this.stripe = Stripe('pk_test_51K3z6VHNHeLGD4Tdl3rmrK0zFo9YxxMcnpHd10CzLa0Tv7eoJqjLnduIIx3CpIWYOj4mE5miTiuv5Sbtv7zemvY0001oaYU8S9')
+            const elements = this.stripe.elements();
+            this.card = elements.create('card', { hidePostalCode: true })
+
+            this.card.mount('#card-element')
+        }
     },
     methods: {
         getItemTotal(item) {
@@ -173,6 +181,59 @@ export default {
             if (this.place === '') {
                 this.errors.push('Algo falta, verificalo.')
             }
+            if (!this.errors.length) {
+                this.$store.commit('setIsLoading', true)
+
+                this.stripe.createToken(this.card).then(result => {                    
+                    if (result.error) {
+                        this.$store.commit('setIsLoading', false)
+
+                        this.errors.push('Algo esta MAAAAL')
+
+                        console.log(result.error.message)
+                    } else {
+                        this.stripeTokenHandler(result.token)
+                    }
+                })
+            }
+        },
+        async stripeTokenHandler(token) {
+            const items = []
+
+            for (let i = 0; i < this.cart.items.length; i++) {
+                const item = this.cart.items[i]
+                const obj = {
+                    product: item.product.id,
+                    quantity: item.quantity,
+                    price: item.product.price * item.quantity
+                }
+
+                items.push(obj)
+            }
+
+            const data = {
+                'first_name': this.first_name,
+                'last_name': this.last_name,
+                'email': this.email,
+                'address': this.address,
+                'zipcode': this.zipcode,
+                'place': this.place,
+                'phone': this.phone,
+                'items': items,
+                'stripe_token': token.id
+            }
+            
+            await axios
+                .post('/api/v1/checkout/', data)
+                .then(response => {
+                    this.$store.commit('clearCart')
+                    this.$router.push('/cart/success')
+                })
+                .catch(error => {
+                    this.errors.push('Algo esta mal. Intentalo de nuevo.')
+                    console.log(error)
+                })
+                this.$store.commit('setIsLoading', false)
         }
     },
     computed: {
